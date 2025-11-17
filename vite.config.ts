ed5b7884a2642b5f9dab1b98d78d5f3cc31d5f40
yamius <yam.bilenko@gmail.com>
2025-11-17 20:05:38 +0000

    update web assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: "/", // ← было './', ставим '/' ИЛИ просто удаляем эту строку
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5000,
   },
   preview: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 5000,
   },
 });
